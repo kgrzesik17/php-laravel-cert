4dc6e7c960aea488a7808ab594f2d2afbcfe8db2
Kacper <kgrzesik17@gmail.com>
2026-09-07 15:08:35 +0200

    all about routing

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Main Page';
+});
+
+Route::get('/xxx', function() {
+    return 'Hello';
+})->name('hello');
+
+Route::get('/greet/{name}', function($name) {
+    return 'Hello ' . $name . '!';
+});
+
+Route::get('/hallo', function() {
+    return redirect()->route('hello');
+});
+
+Route::fallback(function() {
+    return '404!';
 });
