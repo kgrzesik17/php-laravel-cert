3a43913abe5a14d5a7a4c2e11cefa248f799c59f
Kacper <kgrzesik17@gmail.com>
2026-09-08 18:06:40 +0200

    blade templates and directrives

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,15 @@
-Hello Im a Blade template!
+<h1>
+    The list of tasks
+</h1>
 
-@isset($name)
-    <div>The name is: {{ $name }}</div>
-@endisset
+<div>
+    {{-- @if(count($tasks)) --}}
+    @forelse ($tasks as $task)
+    <div>
+        <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
+    </div>
+    @empty
+        <div>There are no tasks</div>
+    @endforelse
+    {{-- @endif --}}
+</div>
