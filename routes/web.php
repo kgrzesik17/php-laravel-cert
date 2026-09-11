4a402f6ed4bca1f727e77b3c6f6a59d81d909e3e
Kacper <kgrzesik17@gmail.com>
2026-09-11 23:12:45 +0200

    forms and csrf protection

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Response;
+Use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function() {
@@ -13,9 +14,15 @@ Route::get('/tasks', function(){
     ]);
 })->name('tasks.index');
 
+Route::view('/tasks/create', 'create')->name('tasks.create');
+
 Route::get('/tasks/{id}', function($id){
     return view('show', ['task' => \App\Models\Task::findOrFail($id)]);
 })->name('tasks.show');
+
+Route::post('/tasks', function(Request $request) {
+    dd($request->all());
+})->name('tasks.store');
 
 // Route::get('/xxx', function() {
 //     return 'Hello';
